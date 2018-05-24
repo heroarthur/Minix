8bb7eba03932a2260487471b101947ef10ea0d72
kwolonciej@gmail.com <you@example.com>
2018-05-24 23:44:32 +0200

    sad

--- minixCode/bootLoader/helloWorld.asm
+++ minixCode/bootLoader/helloWorld.asm
@@ -1,34 +1,7 @@
 mov ah, 0xe 
 mov al, 'H' 
 int 0x10 
-mov ah, 0xe  
-mov al, 'e'                                
-int 0x10
-mov ah, 0xe  
-mov al, 'l'                                
-int 0x10
-mov ah, 0xe  
-mov al, 'l'                                
-int 0x10
-mov ah, 0xe  
-mov al, 'o'                                
-int 0x10
-mov ah, 0xe 
-mov al, ' '                                
-int 0x10
 
-mov ah, 0xe 
-mov al, 'W'                                
-int 0x10 
-mov ah, 0xe  
-mov al, 'r'                                
-int 0x10
-mov ah, 0xe  
-mov al, 'd'                                
-int 0x10
-mov ah, 0xe  
-mov al, '!'                                
-int 0x10
 loop:
     jmp loop
 
