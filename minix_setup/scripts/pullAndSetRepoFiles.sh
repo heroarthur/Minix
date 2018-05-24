33f687690be798d121a3b98567e70865961ca695
kwolonciej@gmail.com <you@example.com>
2018-05-24 22:47:46 +0200

    bug fix

--- minix_setup/scripts/pullAndSetRepoFiles.sh
+++ minix_setup/scripts/pullAndSetRepoFiles.sh
@@ -8,7 +8,7 @@ expect "# "
 send "git clone git://github.com/heroarthur/Minix \r"
 expect "# "
 send "cd Minix && cp -r minixCode ../ && cd .. && rm -r Minix && cd minixCode \r"
-send "./setUpSourceAndTools"
+send "./setUpSourceAndTools \r"
 expect "# "
 #clear terminal 
 send "clear && pwd \r"
